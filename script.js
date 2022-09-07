736c87e41c9f3753d57ade6b80df826e13b3e169
25Alan <romeroalan71@gmail.com>
2022-09-07 16:29:27 -0300

    Finalizado el encriptar y desencriptar. Falta el copy

--- script.js
+++ script.js
@@ -1,21 +1,93 @@
-
-const cifrado = () => {
-  let textInput = document.querySelector("#inputText");
-  let textOutput = document.querySelector("#outputText");
-
-  
-}
+const inputTexto = document.getElementById("inputText");
+const outputText = document.getElementById("outputText");
 
 const buttonInput = document.querySelector("#buttonInput");
-const buttonDesencriptar = document.querySelector("#buttonDesencriptar");
-const buttonCopy = document.querySelector("#buttonCopy");
+const buttonOutput = document.querySelector("#buttonOutput");
 
-buttonInput.addEventListener('click', cifrado);
+function encriptar(stringInput) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
 
-buttonDesencriptar.addEventListener("click", () => {
-  console.log("Presioné boton desencriptar");
+
+  stringInput = stringInput.toLowerCase();
+  stringInput = removeAcentos(stringInput);
+
+  console.log(stringInput);
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringInput.includes(matrizCodigo[i][0])) {
+      stringInput = stringInput.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
+      console.log(stringInput);
+    }
+  }
+  return stringInput;
+}
+
+function desencriptar(stringDesencriptar) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringDesencriptar = stringDesencriptar.toLowerCase();
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptar.includes(matrizCodigo[i][1])) {
+      stringDesencriptar = stringDesencriptar.replaceAll(
+        matrizCodigo[i][1],
+        matrizCodigo[i][0]
+      );
+    }
+  }
+
+  return stringDesencriptar;
+}
+
+function removeAcentos(stringModificado) {
+  var chars = {
+    á: "a",
+    é: "e",
+    í: "i",
+    ó: "o",
+    ú: "u",
+    à: "a",
+    è: "e",
+    ì: "i",
+    ò: "o",
+    ù: "u",
+    ñ: "n",
+    Á: "A",
+    É: "E",
+    Í: "I",
+    Ó: "O",
+    Ú: "U",
+    À: "A",
+    È: "E",
+    Ì: "I",
+    Ò: "O",
+    Ù: "U",
+    Ñ: "N",
+  };
+  var expr = /[áàéèíìóòúùñ]/gi;
+  var res = stringModificado.replace(expr, function (e) {
+    return chars[e];
+  });
+  return res;
+}
+
+buttonInput.addEventListener("click", () => {
+  const textCifrdado = encriptar(inputTexto.value.toLowerCase());
+  outputText.innerHTML = textCifrdado;
 });
 
-buttonCopy.addEventListener("click", () => {
-  console.log("Presioné el botón copiar");
-});
+/* 
+  /^[a-zA-Z\u00C0-\u017F]+$/.test(name))
+*/
